a8c9bf64dff0851bbf3ec8e1e96c08348f1975a6
Stefan Kupresak <stefan_vg@hotmail.com>
2021-08-23 11:16:10 +0200

    refactor html into Alpine.data/2

--- assets/js/logic/BotInterface.js
+++ assets/js/logic/BotInterface.js
@@ -1,3 +1,25 @@
+import Alpine from "alpinejs";
+
+Alpine.data("botInterface", () => ({
+  drawerOpen: null,
+  firstClick: false,
+  handleClick(ev) {
+    this.firstClick = true;
+    this.openDrawer(
+      ev.clientY - ev.target.getBoundingClientRect().top,
+      ev.clientX - ev.target.getBoundingClientRect().left
+    );
+  },
+  openDrawer(top, left) {
+    if (!!this.drawerOpen) return;
+
+    this.drawerOpen = {
+      left,
+      top,
+    };
+  },
+}));
+
 export default {
   mounted() {},
 };
